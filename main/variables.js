ba4d40b74beaa5fed12115994726ff5933ff0b4b
priyanshu gusain <143826023+priyanshugusain2004@users.noreply.github.com>
2024-09-05 03:50:14 +0000

    variables in js

---
+++ main/variables.js
@@ -0,0 +1,14 @@
+const accountid =  14452
+let accountemail = "priyan@google.com"
+var accountpassword = "263116"
+accountcity = "jaipur"
+
+let accountstate
+
+//accountid=2// not allowed
+accountemail= "naanmn@gmail.com"
+accountpassword="gamerpriya"
+
+console.table([accountid,accountpassword,accountemail,accountcity,accountstate]);
+
+
