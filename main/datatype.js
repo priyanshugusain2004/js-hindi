20d82b7466b83f33b2bfe158a29e6c05e69a8e87
priyanshu gusain <143826023+priyanshugusain2004@users.noreply.github.com>
2024-09-06 02:33:23 +0000

    data types

---
+++ main/datatype.js
@@ -0,0 +1,21 @@
+"use strict"// treat all js cde as newer version
+
+//alert(3+3) we r using node.js,not browser 
+
+console.log(3+3); console.log("hello");
+
+//name
+//bigint
+//string
+//bool 
+//null
+//undifine
+//symbol
+
+
+
+//object
+
+console.log(typeof "hello");
+console.log(typeof null);
+
